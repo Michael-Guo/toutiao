49da0f6f04bc15637b3fd5ed33e17bf0ec4e26bc
GWC <kjgwc@126.com>
2016-08-11 22:04:37 +0800

    unit test

--- src/test/java/com/michael/LikeServiceTests.java
+++ src/test/java/com/michael/LikeServiceTests.java
@@ -1,6 +1,9 @@
 package com.michael;
 
+import com.michael.service.LikeService;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,4 +13,50 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ToutiaoApplication.class)
 public class LikeServiceTests {
+
+    @Autowired
+    LikeService likeService;
+
+    //测试用例所在
+    @Test
+    public void testLike() {
+        likeService.like(123, 1, 1);
+        Assert.assertEquals(1, likeService.getLikeStatus(123, 1, 1));
+    }
+
+    @Test
+    public void testDislike() {
+        likeService.disLike(123, 1, 1);
+        Assert.assertEquals(-1, likeService.getLikeStatus(123, 1, 1));
+    }
+
+    //异常测试，正常情况下应该抛出异常
+    @Test(expected = IllegalArgumentException.class)
+    public void testException() {
+        throw new IllegalArgumentException("Exception Test");
+    }
+
+    //在测试用例之前初始化数据
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+    }
+
+    //在测试用例之后清理数据
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
+
+    //在所有测试用例之前运行，只运行一次
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("before");
+    }
+
+    //在所有测试用例之后运行，只运行一次
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("after");
+    }
 }
